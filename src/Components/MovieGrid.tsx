ef5d321552a391376e025a702fea8a54898ccfef
saahilanande <saahilanande21@gmail.com>
2023-08-01 20:15:37 -0500

    Loading skeleton added in movie grid

--- src/Components/MovieGrid.tsx
+++ src/Components/MovieGrid.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import useFetchMovie from "../Hooks/useFetchMovie";
 import MovieDetail from "./MovieDetail";
+import MovieDetailSkeleton from "./MovieDetailSkeleton";
 
 function MovieGrid() {
-  const { movieData, isLoading, isError } = useFetchMovie(0, 20);
+  const { movieData, isLoading, isError } = useFetchMovie(0, 10);
+  let skeletonArray = [1, 2, 3, 4, 5];
   return (
     <>
+      {isLoading ? skeletonArray.map(() => <MovieDetailSkeleton />) : null}
       {movieData.map((data) => (
         <MovieDetail movieData={data} />
       ))}
