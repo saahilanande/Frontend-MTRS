00bfce236ee47c8551c34dcc0abd8dacc92287be
saahilanande <saahilanande21@gmail.com>
2023-08-01 20:20:37 -0500

    Error handled for movie grid

--- src/Components/MovieGrid.tsx
+++ src/Components/MovieGrid.tsx
@@ -2,16 +2,17 @@ import React from "react";
 import useFetchMovie from "../Hooks/useFetchMovie";
 import MovieDetail from "./MovieDetail";
 import MovieDetailSkeleton from "./MovieDetailSkeleton";
+import { Typography } from "@mui/material";
 
 function MovieGrid() {
   const { movieData, isLoading, isError } = useFetchMovie(0, 10);
   let skeletonArray = [1, 2, 3, 4, 5];
   return (
     <>
-      {isLoading ? skeletonArray.map(() => <MovieDetailSkeleton />) : null}
-      {movieData.map((data) => (
-        <MovieDetail movieData={data} />
-      ))}
+      {isError && <Typography variant="h1">{isError}</Typography>}
+      {isLoading
+        ? skeletonArray.map(() => <MovieDetailSkeleton />)
+        : movieData.map((data) => <MovieDetail movieData={data} />)}
     </>
   );
 }
