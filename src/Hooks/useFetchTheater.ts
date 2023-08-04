4396dd109d7f7235271e1f81ae41f0aa36b9ddda
saahilanande <saahilanande21@gmail.com>
2023-08-04 18:16:05 -0500

    custom hook added to fetch theater

---
+++ src/Hooks/useFetchTheater.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import ApiClient from "../Services/Api-Client";
+
+export interface theaterShema {
+
+}
+
+const useFetchTheater = (theaterId:number) => {
+
+    const [theaterData, settheaterData] = useState<theaterShema[]>([]);
+    const [isLoading, setisLoading] = useState(false);
+    const [isError, setIsErrorr] = useState("");
+
+    useEffect(() => {
+        setisLoading(true);
+        ApiClient.get("/theater/"+theaterId)
+          .then((res) => {
+            settheaterData(res.data);
+            setisLoading(false);
+          })
+          .catch((err) => {
+            setIsErrorr(err.message);
+            setisLoading(false);
+          });
+      }, []);
+      return { theaterData, isLoading, isError };
+
+};
+
+export default useFetchTheater;
